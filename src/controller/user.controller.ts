57f0fb0786b2113a1a9df6fa3282db376ed2db29
najafsikander <najafsikander@yahoo.com>
2025-05-02 17:55:00 +0400

    Test

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -17,7 +17,7 @@ export class UserController {
 
     createUser = (req:Request,res:Response) => {
         try {
-            const user = this.userService.createUser(req.body);
+            const user = this.userService.createUser(req.socket);
             res.status(201).json({ message: user });
         } catch (error) {
             warn("Error in controller createUser: ", error);
