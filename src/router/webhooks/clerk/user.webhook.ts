d462426269fa353d6d081b81278c2fc2a430590f
najafsikander <najafsikander@yahoo.com>
2025-05-06 17:47:42 +0400

    Added body parser for webhook

--- src/router/webhooks/clerk/user.webhook.ts
+++ src/router/webhooks/clerk/user.webhook.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { UserController } from "../../../controller/webhooks/user.controller";
+import express from "express";
 
 const router = Router();
+const  rawBody = express.raw({ type: 'application/json' });
 const userController = new UserController();
 
-router.post('/user',userController.updateUser);
+router.post('/user',rawBody,userController.updateUser);
 
 export default router;
